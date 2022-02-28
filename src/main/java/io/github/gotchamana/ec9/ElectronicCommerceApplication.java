b915b234f189b129f2471bf0926dfb195e17fe0b
shootingstar <jinhung.c@gmail.com>
2022-02-28 12:43:16 +0800

    Init repo

---
+++ src/main/java/io/github/gotchamana/ec9/ElectronicCommerceApplication.java
@@ -0,0 +1,12 @@
+package io.github.gotchamana.ec9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicCommerceApplication.class, args);
+    }
+}
